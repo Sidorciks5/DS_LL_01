d5e8bb6ee88a5f435ec6c51dc095b7228a8d3541
Sidorciks_Roberts_241RDC042 <roberts.sidorciks@edu.rtu.lv>
2025-02-18 09:00:46 +0200

    Update LL_2.py

--- LL_2.py
+++ LL_2.py
@@ -24,29 +24,32 @@ class LinkedList:
     def has_loop(self):
         slow = self.head
         fast = self.head
-
-        while fast is not None and fast.next is not None:
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
             if slow == fast:
-                return True  
-
+                return True
         return False
     
     
-
+    
+    
 my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
 my_linked_list_1.append(4)
-my_linked_list_1.tail.next = my_linked_list_1.head 
-print(my_linked_list_1.has_loop())
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop() ) # Returns True
+
+
+
 
 my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
 my_linked_list_2.append(4)
-print(my_linked_list_2.has_loop())
+print(my_linked_list_2.has_loop() ) # Returns False
+
+
 
 
